de731bf60b536d45df4b6348757f3cf3d3d69802
R.Timur <t.rakhmetulla@almau.edu.kz>
2025-02-17 12:32:52 +0500

    LECTURE 5

---
+++ lecture_5/shop/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from shop.models import Category, Product
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+
+class ProductForm(forms.ModelForm):
+    name = forms.CharField(min_length=1, max_length=200, required=True)
+    description = forms.CharField(min_length=0, max_length=2000, required=False)
+    amount = forms.IntegerField(min_value=0, max_value=1000, required=True)
+    price = forms.IntegerField(min_value=0, required=True)
+
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'amount', 'price']
---
+++ lecture_5/shop/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render, redirect
+from .models import Product, Category
+from .forms import ProductForm, CategoryForm
+
+
+def index_category(request):
+    if request.method == 'GET':
+        form = CategoryForm()
+        categories = Category.objects.all()
+        return render(request, 'shop/index.html',
+                      {
+                          'categories': categories,
+                          'form': form
+                      })
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+
+def update_category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    if request.method == 'GET':
+        form = CategoryForm(instance=category)
+        return render(request, 'shop/update_category.html',
+                      {
+                          'category': category,
+                          'form': form
+                      })
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+
+def delete_category(request, category_id):
+    try:
+        category = Category.objects.get(id=category_id)
+        category.delete()
+        return redirect('/')
+    except Product.DoesNotExist as e:
+        return redirect('index')
+
+
+def category_details(request, category_id):
+    category = Category.objects.get(id=category_id)
+    if request.method == 'GET':
+        form = ProductForm()
+        products = Product.objects.filter(category=category_id)
+        return render(request, 'shop/category_details.html',
+                      {
+                          'products': products,
+                          'category': category,
+                          'form': form
+                      })
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            name = form.data.get('name')
+            price = form.data.get('price')
+            amount = form.data.get('amount')
+            description = form.data.get('description')
+            product = Product(
+                name=name,
+                price=price,
+                amount=amount,
+                description=description,
+                category=Category.objects.get(id=category_id),
+            )
+            product.save()
+        return redirect('category_details', category_id)
+
+
+def update_product(request, category_id, product_id):
+    category = Category.objects.get(id=category_id)
+    product = Product.objects.get(id=product_id)
+    if request.method == 'GET':
+        form = ProductForm(instance=product)
+        return render(request, 'shop/update_product.html',
+                      {
+                          'product': product,
+                          'category': category,
+                          'form': form
+                      })
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+        return redirect('category_details', category_id)
+
+
+def products_details(request, category_id, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'shop/product-details.html', {'product': product})
+
+
+def delete_product(request, category_id, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+        product.delete()
+        return redirect('category_details', category_id)
+    except Product.DoesNotExist as e:
+        return redirect('index')
